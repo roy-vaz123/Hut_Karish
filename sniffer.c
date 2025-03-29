1a87dd810d9a55483f44b7b8c61a39e7e711d08e
Roy <royiki22@gmail.com>
2025-03-29 15:32:25 +0300

    added netlink socket create, send, and recieve, the modoule basic version is complete

--- sniffer.c
+++ sniffer.c
@@ -16,6 +16,14 @@
 #include <linux/list.h>
 #include <linux/spinlock.h>
 #include <linux/slab.h>  // needed for kmalloc and kfree
+// Netlink socket
+#include <net/sock.h>
+#include <linux/netlink.h>
+
+// Netlink constants
+#define NETLINK_USER 31 // Netlink protocol number, 31 is in the range reserved for user-defined Netlink protocols
+#define USER_MSG     (NETLINK_USER + 1) // ?
+#define MAX_PAYLOAD  1024  // message size, max size of the payload in bytes
 
 
 // Module information
@@ -26,6 +34,8 @@ MODULE_VERSION("0.1");
 
 
 static struct nf_hook_ops nfho;  // Netfilter hook options struct
+struct sock *nl_sk = NULL; // Netlink socket struct, used to send messages to user space
+
 
 // Packet info struct and list
 struct pckt_info {
@@ -44,7 +54,92 @@ static DEFINE_SPINLOCK(pckt_lock);// Spinlock to protect the list from concurren
 
 
 
-// Retrive ip header from a packet in the socket buffer and printk the packet cought
+// Send a message to user space process by pid using netlink
+static void send_packet_list_to_user(u32 pid)
+{
+    struct pckt_info *entry, *tmp;
+    struct pckt_info *packet_buf;
+    struct sk_buff *skb_out;
+    struct nlmsghdr *nlh;
+    size_t count = 0, total_size, i = 0;
+
+    // Create a Netlink message buffer, spinlock to protect the list from concurrent access to make sure 
+    //the buffer is not modified while we are copying the packets
+    spin_lock(&pckt_lock);
+    // Count how many packets we have
+    list_for_each_entry(entry, &pckt_list, list)
+        count++;
+    if (count == 0)
+        return;  // No packets to send
+
+    total_size = count * sizeof(struct pckt_info);// Calculate the total size of the packet buffer
+    packet_buf = kmalloc(total_size, GFP_ATOMIC); // Allocate a temporary buffer to hold packet data
+    if (!packet_buf)
+        return;
+
+    // Copy all packets into the buffer and clear the list
+    list_for_each_entry_safe(entry, tmp, &pckt_list, list) {
+        packet_buf[i++] = *entry;  
+        list_del(&entry->list); // remove the entry from the list and free it    
+        kfree(entry);              
+    }
+    spin_unlock(&pckt_lock);// The packet_buf is now filled with the packets info
+
+
+    // Create a Netlink message buffer
+    skb_out = nlmsg_new(total_size, GFP_KERNEL);
+    if (!skb_out) {
+        pr_err("sniffer: Failed to allocate Netlink skb\n");
+        kfree(packet_buf);
+        return;
+    }
+    nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, total_size, 0);
+    if (!nlh) {
+        pr_err("sniffer: nlmsg_put failed\n");
+        kfree_skb(skb_out);
+        kfree(packet_buf);
+        return;
+    }
+
+    // Copy our packet buffer into the Netlink message
+    memcpy(nlmsg_data(nlh), packet_buf, total_size);
+    kfree(packet_buf);  // Free the kernel buffer after copying
+
+    // Send the message to user space
+    if (nlmsg_unicast(nl_sk, skb_out, pid) < 0) {
+        pr_err("sniffer: Failed to send Netlink message\n");
+    }
+}
+
+
+// Netlink Receive function (called when a message is received from user space)
+static void nl_recv_msg(struct sk_buff *skb)
+{
+    struct nlmsghdr *nlh;
+    u32 pid;
+    char *user_msg;
+
+    // Extract the Netlink message header
+    nlh = (struct nlmsghdr *)skb->data;
+
+    // Get the sender PID (user-space process ID)
+    pid = nlh->nlmsg_pid;
+
+    // Extract the actual message data
+    user_msg = (char *)nlmsg_data(nlh);
+
+    pr_info("sniffer: received netlink message: %s\n", user_msg);
+
+    // If the message is "get_packets", send the packet list to the user (to the sender proccess)
+    if (strcmp(user_msg, "get_packets") == 0) {
+        send_packet_list_to_user(pid);
+    } else {
+        pr_info("sniffer: Unknown command: %s\n", user_msg);
+    }
+}
+
+
+// Retrive ip header from a packet in the socket buffer and pr_info the packet cought
 static unsigned int packet_sniffer_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state){
     
     // Frame headers structs
@@ -62,7 +157,7 @@ static unsigned int packet_sniffer_hook(void *priv, struct sk_buff *skb, const s
     
     // Check if the skb is NULL or too short (packets comes as sk_buff struct, skb = the packet)
     if (!skb || skb->len < sizeof(struct iphdr)) {
-        printk(KERN_INFO "[sniffer] Packet too short or skb is NULL. \n");
+        pr_info("[sniffer] Packet too short or skb is NULL. \n");
         return NF_ACCEPT;
     }
     
@@ -94,13 +189,13 @@ static unsigned int packet_sniffer_hook(void *priv, struct sk_buff *skb, const s
     
     
     } else { // Our module only supports TCP and UDP packets
-        printk(KERN_INFO "[sniffer] Unsupported protocol: %d \n", iph->protocol);
+        pr_info("[sniffer] Unsupported protocol: %d \n", iph->protocol);
         return NF_ACCEPT;
     }
                 
     
-    // Printk the packet info, for debugging
-    printk(KERN_INFO "[sniffer] Packet type %c Src IP: %pI4, Dst IP: %pI4, Src Port: %u, Dst Port: %u, Payload: %u bytes \n", proto, &src_ip, &dst_ip, src_port, dst_port, payload_size);
+    // pr_info the packet info, for debugging
+    pr_info("[sniffer] Packet type %c Src IP: %pI4, Dst IP: %pI4, Src Port: %u, Dst Port: %u, Payload: %u bytes \n", proto, &src_ip, &dst_ip, src_port, dst_port, payload_size);
  
     entry = kmalloc(sizeof(*entry), GFP_ATOMIC);// Allocate memory for enrty in the list, GFP_ATOMIC prevents sleeping in the kernel space
     if (!entry)// If kmalloc failed
@@ -127,15 +222,29 @@ static unsigned int packet_sniffer_hook(void *priv, struct sk_buff *skb, const s
 
 // init function (runs on module load)
 static int __init sniffer_init(void) {
-    printk(KERN_INFO "[sniffer] Module loaded.\n");
+    pr_info("[sniffer] Module loaded.\n");
 
+    // Create a Netlink socket
+    struct netlink_kernel_cfg cfg = {// Netlink socket configuration
+        .input = nl_recv_msg, 
+    };   
+    nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, &cfg);// Creats the actual socket
+    if (!nl_sk) {
+        pr_err("sniffer: Failed to create Netlink socket\n");
+        return -ENOMEM;
+    }   
+    pr_info("sniffer: Netlink socket created\n");
+    
+
+
+    // Netfilter hook initilaziation registration
     nfho.hook = packet_sniffer_hook;       // The function to run when a packet hits the hook
     nfho.hooknum = NF_INET_LOCAL_IN;    // Places the hook after basec proccessing, the skb header will be complete at this stage, wait for ipv4 packets
     nfho.pf = PF_INET;                     // IPv4 packets only
     nfho.priority = NF_IP_PRI_FIRST;       // Run before other hooks (highest priority)
-
     nf_register_net_hook(&init_net, &nfho); // Register the hook with netfilter (init_net refers to the default network namespace)
 
+    
     return 0;
 }
 
@@ -143,8 +252,8 @@ static int __init sniffer_init(void) {
 static void __exit sniffer_exit(void) {
     // Free the list and its entries
     struct pckt_info *entry, *tmp;
+    
     spin_lock(&pckt_lock);// Lock the list before iterating over it to prevent concurrent access
-
     // Iterate over the list and free each entry
     list_for_each_entry_safe(entry, tmp, &pckt_list, list) {
         list_del(&entry->list);
@@ -153,7 +262,14 @@ static void __exit sniffer_exit(void) {
     spin_unlock(&pckt_lock);
     
     nf_unregister_net_hook(&init_net, &nfho); // Unregister the hook (if the hook is not unregistered, it will remain active even after the module is unloaded) 
-    printk(KERN_INFO "[sniffer] Module unloaded.\n");
+    
+    // Unregister the Netlink socket
+    if (nl_sk) {
+        netlink_kernel_release(nl_sk);
+        pr_info("sniffer: Netlink socket released\n");
+    }
+    
+    pr_info("[sniffer] Module unloaded.\n");
 }
 
 // Register the init and exit functions
--- sniffer.mod.c
+++ sniffer.mod.c
@@ -32,21 +32,29 @@ __used __section("__versions") = {
 	{ 0xbdfb6dbb, "__fentry__" },
 	{ 0x92997ed8, "_printk" },
 	{ 0x1343e6f5, "init_net" },
+	{ 0xbd245904, "__netlink_kernel_create" },
 	{ 0x84bb745e, "nf_register_net_hook" },
+	{ 0xa19b956, "__stack_chk_fail" },
 	{ 0x5b8239ca, "__x86_return_thunk" },
+	{ 0xe2d5255a, "strcmp" },
 	{ 0xba8fbd64, "_raw_spin_lock" },
+	{ 0xeb233a45, "__kmalloc" },
 	{ 0xe1537255, "__list_del_entry_valid" },
 	{ 0x37a0cba, "kfree" },
 	{ 0xb5b54b34, "_raw_spin_unlock" },
-	{ 0x66e1119e, "nf_unregister_net_hook" },
-	{ 0xa19b956, "__stack_chk_fail" },
+	{ 0xdc324703, "__alloc_skb" },
+	{ 0x6de57620, "kfree_skb_reason" },
+	{ 0x461f148, "__nlmsg_put" },
+	{ 0x3d6af433, "netlink_unicast" },
 	{ 0x8d5931c0, "kmalloc_caches" },
 	{ 0xca078420, "kmalloc_trace" },
 	{ 0x68f31cbd, "__list_add_valid" },
+	{ 0x66e1119e, "nf_unregister_net_hook" },
+	{ 0x6ea2152, "netlink_kernel_release" },
 	{ 0x82583a65, "module_layout" },
 };
 
 MODULE_INFO(depends, "");
 
 
-MODULE_INFO(srcversion, "45439F35CAE65D228063AA8");
+MODULE_INFO(srcversion, "B32E9ABB1FB2170F21B6033");
